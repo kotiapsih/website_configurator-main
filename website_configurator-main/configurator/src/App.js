6ff17ae7268c69feae9e7005f5f7c19b85446c61
kotiapsih <kotiapsih@mai.ru>
2022-02-05 14:07:15 +0400

    nkml

---
+++ website_configurator-main/configurator/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Главная страница
+        </p>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Редактировать справку
+        </a>
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Редактировать изображения
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
